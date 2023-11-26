c745503a3a881517a9e57b133f7989b3c42deb8b
shrimann23 <shrima.narasimhan@gmail.com>
2023-11-26 13:20:19 -0800

    Added video, footer, and styled home page

--- app/authors/page.js
+++ app/authors/page.js
@@ -2,39 +2,19 @@ import Container from "../components/Container";
 
 export default async function Authors() {
     const authors = await getAuthors();
-
-    // const author1 = {
-    //     "name": "Charlie Brown",
-    //     "image": "https://www.peanuts.com/sites/default/files/cb-color.jpg",
-    //     "bio": "Charlie Brown needs free therapy!"
-    // }
-    // const author2 = {
-    //     "name": "Sally Brown",
-    //     "image": "https://www.peanuts.com/sites/default/files/sa-color.jpg",
-    //     "bio": "Charlie, we have a problem."
-    // }
-    // const author3 = {
-    //     "name": "Snoopy",
-    //     "image": "https://cdn.theatlantic.com/thumbor/iRIVzWCrLxABf1kjfzOSnIde070=/801x0:3363x2562/1080x1080/media/img/2015/09/BOB_Boxer_Peanuts_Opener_HP/original.jpg",
-    //     "bio": "Snoopy is quite the gentleman!" 
-    // }
-    // const author4 = {
-    //     "name": "Lucy van Pelt",
-    //     "image": "https://s3.amazonaws.com/comicgeeks/characters/avatars/18191.jpg?t=1676946853",
-    //     "bio": "Someone tell Charlie Brown I'm charging five cents for therapy"
-    // }
-
+    console.log(authors);
     return (
         <Container>
             <p>Get to know our Peanuts Blog authors!</p>
-            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {authors.map((author) => (
-                    <AuthorCard key={author.name} author={author}/>
-                ))}
-            </ul>
         </Container>
     );
 }
+
+/* <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {authors.map((author) => (
+                    <AuthorCard key={author.name} author={author} />
+                ))}
+            </ul> */
 
 async function getAuthors() {
 
